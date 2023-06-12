6c3aa63592aa5b521cb26cf5a871745fa4c29e16
Leonardo Alves <leonardoallves1992@gmail.com>
2023-06-12 17:49:23 -0300

    Versao 2.0

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('core.urls'))    # primeiro param vazio para entrar pela index
 ]
+
+# if settings.DEBUG: urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
